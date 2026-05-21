97a5420fe4f71a4dc2a1db25f4906c62659d7f8f
Brayan603 <b.malova60@gmail.com>
2026-05-21 09:34:22 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-//import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -11,7 +10,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-         <Route path="/" element={<Hero />} />
+        <Route path="/" element={<Hero />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
